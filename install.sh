5bd966ec1c3e2455f5bfd3a6b9911ee25bb1d65f
Darius Brown <59292656+MrRedacted@users.noreply.github.com>
2024-10-21 15:51:17 -0400

    Update install.sh

--- install.sh
+++ install.sh
@@ -2,7 +2,7 @@
 
 wget -O "$HOME/.dbash.sh" 'https://raw.githubusercontent.com/MrRedacted/dbash/master/.dbash.sh' &&
   if [ -f "$HOME/.bash_profile" ]; then
-    printf '\n. "$HOME/.dbash.sh"' >>"$HOME/.bash_profile"
+    printf '\n. "$HOME/.dbash.sh"\n' >>"$HOME/.bash_profile"
   else
-    printf '\n. "$HOME/.dbash.sh"' >>"$HOME/.profile"
+    printf '\n. "$HOME/.dbash.sh"\n' >>"$HOME/.profile"
   fi
